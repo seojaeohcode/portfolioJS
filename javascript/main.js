9361199a50d715a69fb5823d533d6f03e0b46757
seojaeoh <seojaeohcode@gmail.com>
2022-08-10 22:31:37 +0900

    commit

--- javascript/main.js
+++ javascript/main.js
@@ -4,40 +4,77 @@ const menu = document.querySelector('#menu');
 let menubar = document.querySelector('#menubar');
 let isMenu = false;
 
-// let 
+let next = document.querySelector('#next');
+let prev = document.querySelector('#prev');
+let allowSkip = true;
+
+console.log(next);
+console.log(prev);
+
+// 인용사진 슬라이더
+const quoteAnim = ['../asset/quote/1.jpg', '../asset/quote/2.jpg', '../asset/quote/3.jpg', '../asset/quote/4.jpg', '../asset/quote/5.jpg', '../asset/quote/6.jpg', '../asset/quote/7.jpg', '../asset/quote/8.jpg', '../asset/quote/9.jpg', '../asset/quote/10.jpg']
+let j = 1;
+document.querySelector('.slideArea>img').src = quoteAnim[0];
 
 // 헤더 텍스트
-const textAnim = 
-['M','Y','\u00a0','B','o','o','k','&','L','e','c','t','u','r','e',]
+const textAnim =
+    ['M', 'Y', '\u00a0', 'B', 'o', 'o', 'k', '&', 'L', 'e', 'c', 't', 'u', 'r', 'e',]
 let i = 0;
-console.log(HeadText);
+
 
 setInterval(() => {
-    if(i == 0){
+    if (i == 0) {
         HeadText.innerText = textAnim[0];
         i++;
     }
-    else if(i < textAnim.length){
+    else if (i < textAnim.length) {
         HeadText.innerText += textAnim[i];
         i++;
     }
-    else if(i>=textAnim.length){
-        i=0;
+    else if (i >= textAnim.length) {
+        i = 0;
         HeadText.innerText = '\u00a0';
     }
 }, 500);
 
 // 메뉴 버튼
-menu.addEventListener('click', function() {
-    if(isMenu == false){
+menu.addEventListener('click', function () {
+    if (isMenu == false) {
         // menubar.style.display = 'block';
         $('#menubar').slideDown(1000);
         isMenu = true;
     }
-    else if(isMenu == true){
+    else if (isMenu == true) {
         // menubar.style.display = 'none';
         $('#menubar').slideUp(1000);
         isMenu = false;
     }
-})
+});
 
+next.addEventListener('click', function () {
+
+    $(".slideArea>img").animate({ opacity: '0' },500,
+    function() {
+        j++;
+        if (j > 9) {
+            j = 0;
+        }
+        document.querySelector('.slideArea>img').src = quoteAnim[j];
+    });
+
+    $(".slideArea>img").animate({ opacity: '1' }, 500,
+        function () {
+            $('.slideArea>img').clearQueue();
+        });
+});
+
+prev.addEventListener('click', function () {
+    j--;
+    if (j < 0) {
+        j = 9;
+    }
+
+    $(".slideArea>img").fadeOut();
+    document.querySelector('.slideArea>img').src = quoteAnim[j];
+    $(".slideArea>img").fadeIn();
+});
