82f4d08f57ab4186bb1d54edf2bc402962ec1af7
seojaeohcode <seojaeohcoder@gmail.com>
2022-08-15 18:13:51 +0900

    commit

--- javascript/books.js
+++ javascript/books.js
@@ -10,7 +10,18 @@ let isMenu = false;
 
 let data = [];
 // localStorage.setItem(`dataIndex`, localStorage.length);
-let dataIndex = localStorage.length;
+let max = 0;
+for (let i = 0; i < localStorage.length; i++) {
+  let key = localStorage.key(i);
+  let item = localStorage.getItem(key);
+  let array = item.split(",");
+  let newMax = Number(array[5]);
+  if (max < newMax) {
+    max = newMax;
+  }
+}
+
+let dataIndex = max + 1;
 let delBtn = 0;
 let checkBtn = 0;
 let datanum = 0;
@@ -93,204 +104,261 @@ setInterval(() => {
   document.querySelector("#mobileDate").innerText = hour + ":" + minute;
 }, 1000);
 
+let sort = [];
+let repeat = localStorage.length;
+
+for (let i = 0; i < localStorage.length; i++) {
+  // console.log("IN");
+  // let key2 = localStorage.key(i);
+  // let item2 = localStorage.getItem(key2);
+  // let array = item2.split(",");
+  // let min = Number(array[5]);
+
+  // for (let j = localStorage.length - repeat; j < localStorage.length; j++) {
+  //   let key = localStorage.key(j);
+  //   let item = localStorage.getItem(key);
+  //   let array = item.split(",");
+  //   let newMin = Number(array[5]);
+
+  //   if (min > newMin) {
+  //     min = newMin;
+  //   }
+  // }
+  // repeat--;
+  // console.log("OUT");
+  // sort.push(min);
+  // console.log(sort);
+  let key2 = localStorage.key(i);
+  let item2 = localStorage.getItem(key2);
+  let array = item2.split(",");
+  let num = Number(array[5]);
+  sort.push(num);
+}
+
+sort.sort();
+
 // 새로고침 후 화면유지
 if (localStorage.length > 0) {
   for (let i = 0; i < localStorage.length; i++) {
-    let key = localStorage.key(i);
-    let current = localStorage.getItem(key);
-    // console.log(current);
-    let array = current.split(",");
-    // console.log(array);
+    for (let j = 0; j < localStorage.length; j++) {
+      let key = localStorage.key(j);
+      let current = localStorage.getItem(key);
+      console.log(current);
+      let array = current.split(",");
 
-    let div = document.createElement("div");
+      let Itemnum = sort[i];
+      if (array[5] == Itemnum) {
+        // let key = localStorage.key(i);
+        // let current = localStorage.getItem(key);
+        // // console.log(current);
+        // let array = current.split(",");
+        // // console.log(array);
 
-    let numberRing = document.createElement("p");
-    let num = array[5];
-    let numberRingTextNode = document.createTextNode(num);
+        let div = document.createElement("div");
 
-    let bookImg = document.createElement("img");
-    bookImg.src = array[0] + "," + array[1];
+        let numberRing = document.createElement("p");
+        let num = array[5];
+        let numberRingTextNode = document.createTextNode(num);
 
-    let p1 = document.createElement("p");
-    let p1TextNode = document.createTextNode(array[2]);
+        let bookImg = document.createElement("img");
+        bookImg.src = array[0] + "," + array[1];
 
-    let p2 = document.createElement("p");
-    let p2TextNode = document.createTextNode("Auther: " + array[3]);
+        let p1 = document.createElement("p");
+        let p1TextNode = document.createTextNode(array[2]);
 
-    let form = document.createElement("form");
-    let checkbox = document.createElement("input");
-    checkbox.setAttribute("type", "checkbox");
-    // console.log(array);
-    checkbox.id = "read";
-    // array[3] == true ? checkbox.checked = true : checkbox.checked = false;
-    array[4] == "true" ? (checkbox.checked = true) : (checkbox.checked = false);
+        let p2 = document.createElement("p");
+        let p2TextNode = document.createTextNode("Auther: " + array[3]);
 
-    let span = document.createElement("span");
-    let spanTxt = document.createTextNode("Read: ");
+        let form = document.createElement("form");
+        let checkbox = document.createElement("input");
+        checkbox.setAttribute("type", "checkbox");
+        // console.log(array);
+        checkbox.id = "read";
+        // array[3] == true ? checkbox.checked = true : checkbox.checked = false;
+        array[4] == "true"
+          ? (checkbox.checked = true)
+          : (checkbox.checked = false);
 
-    let Delete = document.createElement("button");
-    Delete.id = "DeleteBook";
-    Delete.textContent = "Delete";
+        let span = document.createElement("span");
+        let spanTxt = document.createTextNode("Read: ");
 
-    if (mobile.matches === true) {
-      div.style.width = "90%";
-      div.style.margin = "3vw auto";
-      div.style.textAlign = "center";
-      div.style.backgroundColor = "#F5E8C7";
-      div.style.borderRadius = "10px";
-      div.style.marginTop = "4vw";
-      div.style.fontSize = "10vw";
-      div.style.color = "black";
-      // div.style.fontWeight = "bold";
-      div.style.fontFamily = "'EB Garamond', serif";
-      div.style.boxShadow = "0px 5px 5px white";
+        let Delete = document.createElement("button");
+        Delete.id = "DeleteBook";
+        Delete.textContent = "Delete";
 
-      // numberRing.style.paddingTop = "3vw";
-      numberRing.style.display = "none";
+        if (mobile.matches === true) {
+          div.style.width = "90%";
+          div.style.margin = "3vw auto";
+          div.style.textAlign = "center";
+          div.style.backgroundColor = "#F5E8C7";
+          div.style.borderRadius = "10px";
+          div.style.marginTop = "4vw";
+          div.style.fontSize = "10vw";
+          div.style.color = "black";
+          // div.style.fontWeight = "bold";
+          div.style.fontFamily = "'EB Garamond', serif";
+          div.style.boxShadow = "0px 5px 5px white";
 
-      p1.style.paddingTop = "3vw";
-      p1.style.textShadow = "0px 2px 2px gray";
-      p2.style.textShadow = "0px 2px 2px gray";
+          // numberRing.style.paddingTop = "3vw";
+          numberRing.style.display = "none";
 
-      span.style.textShadow = "0px 2px 2px gray";
-      // span.style.verticalAlign = "middle";
+          p1.style.paddingTop = "3vw";
+          p1.style.textShadow = "0px 2px 2px gray";
+          p2.style.textShadow = "0px 2px 2px gray";
 
-      bookImg.style.width = "80vw";
-      bookImg.style.marginTop = "4vw";
+          span.style.textShadow = "0px 2px 2px gray";
+          // span.style.verticalAlign = "middle";
 
-      checkbox.style.width = "5.5vw";
-      checkbox.style.height = "5.5vw";
-      checkbox.style.verticalAlign = "middle";
-      checkbox.style.marginBottom = "0.5rem";
+          bookImg.style.width = "80vw";
+          bookImg.style.marginTop = "4vw";
 
-      Delete.style.marginBottom = "4vw";
-      Delete.style.width = "20vw";
-      Delete.style.height = "10vw";
-      Delete.style.border = "1px solid black";
-      Delete.style.borderRadius = "1rem";
-      Delete.style.background = "transparent";
-      Delete.style.boxShadow = "0px 5px 5px black";
-      Delete.style.fontSize = "6vw";
-      Delete.style.fontFamily = "'EB Garamond', serif";
-    } else if (tablet.matches === true) {
-      div.style.width = "90%";
-      div.style.margin = "3vw auto";
-      div.style.textAlign = "center";
-      div.style.backgroundColor = "#F5E8C7";
-      div.style.borderRadius = "10px";
-      div.style.marginTop = "4vw";
-      div.style.fontSize = "10vw";
-      div.style.color = "black";
-      // div.style.fontWeight = "bold";
-      div.style.fontFamily = "'EB Garamond', serif";
-      div.style.boxShadow = "0px 5px 5px white";
+          checkbox.style.width = "5.5vw";
+          checkbox.style.height = "5.5vw";
+          checkbox.style.verticalAlign = "middle";
+          checkbox.style.marginBottom = "0.5rem";
 
-      // numberRing.style.paddingTop = "3vw";
-      numberRing.style.display = "none";
+          Delete.style.marginBottom = "4vw";
+          Delete.style.width = "20vw";
+          Delete.style.height = "10vw";
+          Delete.style.border = "1px solid black";
+          Delete.style.borderRadius = "1rem";
+          Delete.style.background = "transparent";
+          Delete.style.boxShadow = "0px 5px 5px black";
+          Delete.style.fontSize = "6vw";
+          Delete.style.fontFamily = "'EB Garamond', serif";
+        } else if (tablet.matches === true) {
+          div.style.width = "90%";
+          div.style.margin = "3vw auto";
+          div.style.textAlign = "center";
+          div.style.backgroundColor = "#F5E8C7";
+          div.style.borderRadius = "10px";
+          div.style.marginTop = "4vw";
+          div.style.fontSize = "10vw";
+          div.style.color = "black";
+          // div.style.fontWeight = "bold";
+          div.style.fontFamily = "'EB Garamond', serif";
+          div.style.boxShadow = "0px 5px 5px white";
 
-      p1.style.paddingTop = "3vw";
-      p1.style.textShadow = "0px 2px 2px gray";
-      p2.style.textShadow = "0px 2px 2px gray";
+          // numberRing.style.paddingTop = "3vw";
+          numberRing.style.display = "none";
 
-      span.style.textShadow = "0px 2px 2px gray";
-      // span.style.verticalAlign = "middle";
+          p1.style.paddingTop = "3vw";
+          p1.style.textShadow = "0px 2px 2px gray";
+          p2.style.textShadow = "0px 2px 2px gray";
 
-      bookImg.style.width = "80vw";
-      bookImg.style.marginTop = "4vw";
+          span.style.textShadow = "0px 2px 2px gray";
+          // span.style.verticalAlign = "middle";
 
-      checkbox.style.width = "5.5vw";
-      checkbox.style.height = "5.5vw";
-      checkbox.style.verticalAlign = "middle";
-      checkbox.style.marginBottom = "0.5rem";
+          bookImg.style.width = "80vw";
+          bookImg.style.marginTop = "4vw";
 
-      Delete.style.marginBottom = "4vw";
-      Delete.style.width = "20vw";
-      Delete.style.height = "10vw";
-      Delete.style.border = "1px solid black";
-      Delete.style.borderRadius = "1rem";
-      Delete.style.background = "transparent";
-      Delete.style.boxShadow = "0px 5px 5px black";
-      Delete.style.fontSize = "6vw";
-      Delete.style.fontFamily = "'EB Garamond', serif";
-    } else if (pc.matches === true) {
-      div.style.width = "90%";
-      div.style.margin = "3vw auto";
-      div.style.textAlign = "center";
-      div.style.backgroundColor = "#F5E8C7";
-      div.style.borderRadius = "10px";
-      div.style.marginTop = "4vw";
-      div.style.fontSize = "10vw";
-      div.style.color = "black";
-      // div.style.fontWeight = "bold";
-      div.style.fontFamily = "'EB Garamond', serif";
-      div.style.boxShadow = "0px 5px 5px white";
+          checkbox.style.width = "5.5vw";
+          checkbox.style.height = "5.5vw";
+          checkbox.style.verticalAlign = "middle";
+          checkbox.style.marginBottom = "0.5rem";
 
-      // numberRing.style.paddingTop = "3vw";
-      numberRing.style.display = "none";
+          Delete.style.marginBottom = "4vw";
+          Delete.style.width = "20vw";
+          Delete.style.height = "10vw";
+          Delete.style.border = "1px solid black";
+          Delete.style.borderRadius = "1rem";
+          Delete.style.background = "transparent";
+          Delete.style.boxShadow = "0px 5px 5px black";
+          Delete.style.fontSize = "6vw";
+          Delete.style.fontFamily = "'EB Garamond', serif";
+        } else if (pc.matches === true) {
+          div.style.width = "90%";
+          div.style.margin = "3vw auto";
+          div.style.textAlign = "center";
+          div.style.backgroundColor = "#F5E8C7";
+          div.style.borderRadius = "10px";
+          div.style.marginTop = "4vw";
+          div.style.fontSize = "10vw";
+          div.style.color = "black";
+          // div.style.fontWeight = "bold";
+          div.style.fontFamily = "'EB Garamond', serif";
+          div.style.boxShadow = "0px 5px 5px white";
 
-      p1.style.paddingTop = "3vw";
-      p1.style.textShadow = "0px 2px 2px gray";
-      p2.style.textShadow = "0px 2px 2px gray";
+          // numberRing.style.paddingTop = "3vw";
+          numberRing.style.display = "none";
 
-      span.style.textShadow = "0px 2px 2px gray";
-      // span.style.verticalAlign = "middle";
+          p1.style.paddingTop = "3vw";
+          p1.style.textShadow = "0px 2px 2px gray";
+          p2.style.textShadow = "0px 2px 2px gray";
 
-      bookImg.style.width = "80vw";
-      bookImg.style.marginTop = "4vw";
+          span.style.textShadow = "0px 2px 2px gray";
+          // span.style.verticalAlign = "middle";
 
-      checkbox.style.width = "5.5vw";
-      checkbox.style.height = "5.5vw";
-      checkbox.style.verticalAlign = "middle";
-      checkbox.style.marginBottom = "0.5rem";
+          bookImg.style.width = "80vw";
+          bookImg.style.marginTop = "4vw";
 
-      Delete.style.marginBottom = "4vw";
-      Delete.style.width = "20vw";
-      Delete.style.height = "10vw";
-      Delete.style.border = "1px solid black";
-      Delete.style.borderRadius = "1rem";
-      Delete.style.background = "transparent";
-      Delete.style.boxShadow = "0px 5px 5px black";
-      Delete.style.fontSize = "6vw";
-      Delete.style.fontFamily = "'EB Garamond', serif";
+          checkbox.style.width = "5.5vw";
+          checkbox.style.height = "5.5vw";
+          checkbox.style.verticalAlign = "middle";
+          checkbox.style.marginBottom = "0.5rem";
+
+          Delete.style.marginBottom = "4vw";
+          Delete.style.width = "20vw";
+          Delete.style.height = "10vw";
+          Delete.style.border = "1px solid black";
+          Delete.style.borderRadius = "1rem";
+          Delete.style.background = "transparent";
+          Delete.style.boxShadow = "0px 5px 5px black";
+          Delete.style.fontSize = "6vw";
+          Delete.style.fontFamily = "'EB Garamond', serif";
+        }
+
+        numberRing.appendChild(numberRingTextNode);
+        p1.appendChild(p1TextNode);
+        p2.appendChild(p2TextNode);
+        span.appendChild(spanTxt);
+        form.appendChild(span);
+        form.appendChild(checkbox);
+
+        div.appendChild(p1);
+        div.appendChild(numberRing);
+        div.appendChild(bookImg);
+        div.appendChild(p2);
+        div.appendChild(form);
+        div.appendChild(Delete);
+        document.querySelector("main").appendChild(div);
+
+        // numberRing.appendChild(numberRingTextNode);
+        // p1.appendChild(p1TextNode);
+        // p2.appendChild(p2TextNode);
+        // span.appendChild(spanTxt);
+        // form.appendChild(span);
+        // form.appendChild(checkbox);
+
+        // div.appendChild(p1);
+        // div.appendChild(numberRing);
+        // div.appendChild(bookImg);
+        // div.appendChild(p2);
+        // div.appendChild(form);
+        // div.appendChild(Delete);
+
+        delBtn = document.querySelectorAll("#DeleteBook");
+        checkBtn = document.querySelectorAll("#read");
+        // console.log(delBtn);
+      }
     }
-
-    numberRing.appendChild(numberRingTextNode);
-    p1.appendChild(p1TextNode);
-    p2.appendChild(p2TextNode);
-    span.appendChild(spanTxt);
-    form.appendChild(span);
-    form.appendChild(checkbox);
-
-    div.appendChild(p1);
-    div.appendChild(numberRing);
-    div.appendChild(bookImg);
-    div.appendChild(p2);
-    div.appendChild(form);
-    div.appendChild(Delete);
-    document.querySelector("main").appendChild(div);
-
-    // numberRing.appendChild(numberRingTextNode);
-    // p1.appendChild(p1TextNode);
-    // p2.appendChild(p2TextNode);
-    // span.appendChild(spanTxt);
-    // form.appendChild(span);
-    // form.appendChild(checkbox);
-
-    // div.appendChild(p1);
-    // div.appendChild(numberRing);
-    // div.appendChild(bookImg);
-    // div.appendChild(p2);
-    // div.appendChild(form);
-    // div.appendChild(Delete);
-
-    delBtn = document.querySelectorAll("#DeleteBook");
-    checkBtn = document.querySelectorAll("#read");
-    // console.log(delBtn);
   }
 }
 
 // 새입력
 document.querySelector("#submit").addEventListener("click", function () {
+  max = 0;
+  for (let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+    let item = localStorage.getItem(key);
+    let array = item.split(",");
+    let newMax = Number(array[5]);
+    if (max < newMax) {
+      max = newMax;
+    }
+  }
+  dataIndex = max + 2;
+
   let img = document.querySelector("#bookImage").files[0];
   //   console.log(img);
   let title = document.querySelector("#title").value;
@@ -319,6 +387,7 @@ document.querySelector("#submit").addEventListener("click", function () {
       // 데이터삽입
       let book = [url, title, auther, reading, dataIndex];
       dataIndex++;
+      // console.log(dataIndex);
       data.push(book);
 
       let div = document.createElement("div");
@@ -467,6 +536,13 @@ document.querySelector("#submit").addEventListener("click", function () {
       // console.log(delBtn);
       // console.log(checkBtn);
 
+      for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        let current = localStorage.getItem(key);
+        let array = current.split(",");
+        console.log(array);
+      }
+
       for (let i = 0; i < delBtn.length; i++) {
         delBtn[i].addEventListener("click", removeItem);
       }
@@ -496,37 +572,49 @@ function removeItem(e) {
 
   // 지운 아이템의 넘버링 가져오기
   let delItemNumber = n.childNodes[1].textContent;
-  
-//   n.remove();
 
-//   dataIndex = localStorage.length;
+  //   n.remove();
 
-  for (let i = delItemNumber+1; i < localStorage.length; i++) {
-    console.log("hello");
-    // let key1 = localStorage.key(i-1);//삭제할 노드의 인덱스
-    let key = localStorage.key(i);//이동노드 1번
-    let Node = localStorage.getItem(key);
-    
-    let array = Node.split(",");
-    array[5] = array[5] - 1; 
-    console.log(array);
+  //   dataIndex = localStorage.length;
+  // console.log(delItemNumber+1);
+  // console.log(localStorage.length);
+  //   for (let i = delItemNumber+1; i < localStorage.length; i++) {
+  //     console.log("hello");
+  //     // let key1 = localStorage.key(i-1);//삭제할 노드의 인덱스
+  //     let key = localStorage.key(i);//이동노드 1번
+  //     let Node = localStorage.getItem(key);
 
-    // localStorage.setItem(`${i-1}`, array);
+  //     let array = Node.split(",");
+  //     array[5] = array[5] - 1;
+  //     console.log(array);
+
+  //     // localStorage.setItem(`${i-1}`, array);
+  //   }
+  max = 0;
+  for (let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+    let item = localStorage.getItem(key);
+    let array = item.split(",");
+    let newMax = Number(array[5]);
+    if (max < newMax) {
+      max = newMax;
+    }
   }
+  dataIndex = max + 1;
 
   localStorage.removeItem(`${delItemNumber}`);
-  
+
   n.remove();
 
-  dataIndex = localStorage.length;
+  // dataIndex = 0;
 
   //전체확인
-//   for (let i = 0; i < localStorage.length; i++) {
-//     let key = localStorage.key(i);
-//     let current = localStorage.getItem(key);
-//     let array = current.split(",");
-//     console.log(array);
-//   }
+  //   for (let i = 0; i < localStorage.length; i++) {
+  //     let key = localStorage.key(i);
+  //     let current = localStorage.getItem(key);
+  //     let array = current.split(",");
+  //     console.log(array);
+  //   }
 }
 
 // 체크박스 변경
